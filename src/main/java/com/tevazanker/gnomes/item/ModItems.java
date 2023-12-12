c5af04f4468397e1b2ab58cac5e30987488c06cc
tevaz <tzanker@uwaterloo.ca>
2023-12-12 12:38:09 -0800

    Init commit mushroom block skin broken

---
+++ src/main/java/com/tevazanker/gnomes/item/ModItems.java
@@ -0,0 +1,25 @@
+package com.tevazanker.gnomes.item;
+
+import com.tevazanker.gnomes.Gnomes;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public  static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, Gnomes.MOD_ID);
+
+    public static final RegistryObject<Item> SHORT_MUSHROOM = ITEMS.register("short_mushroom",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_BREWING)));
+
+
+    public static void  register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+
+
+
+}
