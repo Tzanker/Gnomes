e7e1c127ad6a8d675c4ca5055f80e953db34aaf9
tevaz <tzanker@uwaterloo.ca>
2023-12-12 12:40:21 -0800

    Init commit mushroom block skin broken

---
+++ src/main/java/com/tevazanker/gnomes/Gnomes.java
@@ -0,0 +1,42 @@
+package com.tevazanker.gnomes;
+
+import com.mojang.logging.LogUtils;
+import com.tevazanker.gnomes.block.ModBlocks;
+import com.tevazanker.gnomes.item.ModItems;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(Gnomes.MOD_ID)
+public class Gnomes
+{
+    // Directly reference a slf4j logger
+    private static final Logger LOGGER = LogUtils.getLogger();
+    public static final java.lang.String MOD_ID = "gnomes";
+
+    public Gnomes()
+    {
+
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(eventBus);
+        ModBlocks.register(eventBus);
+
+        eventBus.addListener(this::setup);
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event)
+    {
+        // some preinit code
+        LOGGER.info("HELLO FROM PREINIT");
+        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
